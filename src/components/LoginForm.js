73e3edeac23fc21f43311a4e461d9bbf64479148
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-11-20 16:25:28 -0500

    commit tree 348267b9a70913003ea742e683877fee4e78f40b

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,36 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      
+    };
+  }
+
+ handleLogin = (e) => {
+   this.setState({[e.target.name]: e.target.value})
+ }
+
+ loginHandler = (e) => {
+    e.preventDefault()
+    console.log()
+     if (this.state.username && this.state.password){
+       this.props.handleLogin(this.state)
+      }   
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.loginHandler}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleLogin}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleLogin}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,15 +4,31 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+  
+  messageHandler = (e) => {
+    this.setState({message: e.target.value}, ()=>console.log(this.state.message))
+  }
+
+  keepingCount = () => {
+    return this.state.message ? this.props.maxChars - this.state.message.length : this.props.maxChars
   }
 
   render() {
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <div>
+          <strong>Your message:</strong>
+          <input type="text" name="message" id="message" value={this.state.message} onChange={this.messageHandler}/>
+        </div>
+        <div>
+          <p>Characters Remaining: {this.keepingCount()}</p>
+        </div>
       </div>
+      
     );
   }
 }
